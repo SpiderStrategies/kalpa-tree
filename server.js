581bd3779a57dcd916526d5b7b3105fa29b4d9fe
Nathan Bowser <nathan.bowser@spiderstrategies.com>
2014-05-09 06:22:18 -0400

    Add @jerryorr flattened tree

--- server.js
+++ server.js
@@ -3,20 +3,28 @@ var http = require('http')
   , url = require('url')
   , d3 = require('d3')
   , tree = require('./tree')
+  , flat = require('./tree-flat')
 
 http.createServer(function (req, res) {
   if (req.url == '/index.html' || req.url == '/') {
     return fs.createReadStream('./index.html').pipe(res)
-  }
-  if (req.url == '/bundle.js') {
+  } else if (req.url == '/bundle.js') {
     return fs.createReadStream('./bundle.js').pipe(res)
+  } else if (req.url == '/tree.json') {
+    res.end(JSON.stringify(d3.layout.tree().nodes(tree(url.parse(req.url, true).query.depth || 5)), function (key, value) {
+      if (key === 'parent') {
+        return value.id
+      } else {
+        return value
+      }
+    }))
+  } else if (req.url == '/tree-flat.json') {
+    res.end(JSON.stringify(flat()))
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/plain'})
+    res.write('Not found')
+    res.end()
   }
 
-  res.end(JSON.stringify(d3.layout.tree().nodes(tree(url.parse(req.url, true).query.depth || 5)), function (key, value) {
-    if (key === 'parent') {
-      return value.id
-    } else {
-      return value
-    }
-  }))
+
 }).listen(3000)
