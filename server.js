30a3bda408411caa8f08a177203dd8a7a167aaf4
Nathan Bowser <nathan.bowser@spiderstrategies.com>
2014-05-09 06:26:08 -0400

    robust urls

--- server.js
+++ server.js
@@ -6,19 +6,22 @@ var http = require('http')
   , flat = require('./tree-flat')
 
 http.createServer(function (req, res) {
-  if (req.url == '/index.html' || req.url == '/') {
+  var _url = url.parse(req.url, true)
+    , path = _url.pathname
+
+  if (path == '/index.html' || path == '/') {
     return fs.createReadStream('./index.html').pipe(res)
-  } else if (req.url == '/bundle.js') {
+  } else if (path == '/bundle.js') {
     return fs.createReadStream('./bundle.js').pipe(res)
-  } else if (req.url == '/tree.json') {
-    res.end(JSON.stringify(d3.layout.tree().nodes(tree(url.parse(req.url, true).query.depth || 5)), function (key, value) {
+  } else if (path == '/tree.json') {
+    res.end(JSON.stringify(d3.layout.tree().nodes(tree(_url.query.depth || 5)), function (key, value) {
       if (key === 'parent') {
         return value.id
       } else {
         return value
       }
     }))
-  } else if (req.url == '/tree-flat.json') {
+  } else if (path == '/tree-flat.json') {
     res.end(JSON.stringify(flat()))
   } else {
     res.writeHead(404, {'Content-Type': 'text/plain'})
