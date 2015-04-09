19e9977c68ebaabf6d13fccc4fad817ea71f2a40
Nathan Bowser <nathan.bowser@spiderstrategies.com>
2015-04-09 12:44:55 -0400

    Adds test for disable animations if there are too many children.

--- test/tree-drawing-test.js
+++ test/tree-drawing-test.js
@@ -87,5 +87,21 @@ test('emits stream on errors on tree', function (t) {
   })
 
   tree.render()
+})
 
+test('disables animations if opts.maxAnimatable is exceeded', function (t) {
+  var tree = new Tree({stream: stream(), maxAnimatable: 3}).render()
+    , tick = process.nextTick
+
+  process.nextTick = function (fn) {
+    process.nextTick = tick // back to normal
+
+    t.ok(tree.node.classed('notransition'), 'tree nodes have notransition class applied')
+    fn()
+    t.ok(!tree.node.classed('notransition'), 'tree nodes notransition class removed')
+    tree.el.remove()
+    t.end()
+  }
+  t.equal(tree.get(1003)._children.length, 10, 'has 10 hidden nodes')
+  tree.select(1003)
 })
