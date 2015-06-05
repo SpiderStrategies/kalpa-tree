076af28a38e45f88a28d2f7da3d6d5ca7faa181c
Nathan Bowser <nathan.bowser@spiderstrategies.com>
2015-06-05 15:08:26 -0400

    Adds a test for #167
    This test is pretty awesome, FWIW.

--- test/forest-tree-test.js
+++ test/forest-tree-test.js
@@ -129,6 +129,45 @@ test('moves a node to a new root', function (t) {
   })
 })
 
+test('dnd allows a root nodes to change order', function (t) {
+  var s = stream()
+    , tree = new Tree({stream: s, forest: true}).render()
+    , dnd = new Dnd(tree)
+
+  s.on('end', function () {
+    var node = tree.node[0][2]
+      , data = tree._layout[1003]
+    d3.event = new Event
+    d3.event.sourceEvent = new Event
+
+    t.equal(tree.root.length, 2, 'two root nodes to start')
+
+    tree.editable()
+    dnd.start.apply(node, [data, 2])
+    d3.event.y = 5
+    dnd.drag.apply(node, [data, 2])
+
+    dnd.end.apply(node, [data, 2])
+    t.equal(tree.root.length, 3, 'three root nodes')
+
+    var rootOrder = tree.root.map(function (node) { return node.id })
+    t.deepEqual(rootOrder, [1003, 1001, 1002], 'root order is 1003, 1001, 1002')
+
+    // Now move 1002 to the top
+    node = tree.node[0][2]
+    data = tree._layout[1002]
+    dnd.start.apply(node, [data, 2])
+    for (var i = data._y; i >= 0; i--) {
+      d3.event.y = i
+      dnd.drag.apply(node, [data, 2])
+    }
+    dnd.end.apply(node, [data, 2])
+    var newOrder = tree.root.map(function (node) { return node.id })
+    t.deepEqual(newOrder, [1002, 1003, 1001 ], 'new root order is 1002, 1003, 1001')
+    t.end()
+  })
+})
+
 test('dnd allows a node to become a new root', function (t) {
   var s = stream()
     , tree = new Tree({stream: s, forest: true}).render()
